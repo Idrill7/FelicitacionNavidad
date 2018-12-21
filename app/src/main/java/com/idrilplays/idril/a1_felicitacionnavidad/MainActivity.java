a0266d68650258b3419578633c0bda4c22705de5
idril7 <alexuca@hotmail.es>
2018-12-21 02:09:14 +0100

    Version con imagen y fuente cambiada

--- app/src/main/java/com/idrilplays/idril/a1_felicitacionnavidad/MainActivity.java
+++ app/src/main/java/com/idrilplays/idril/a1_felicitacionnavidad/MainActivity.java
@@ -1,7 +1,10 @@
 package com.idrilplays.idril.a1_felicitacionnavidad;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Creamos un objeto TypeFace, a este le decimos que de los asset obtenga la fuente
+        Typeface miFuente  = Typeface.createFromAsset(getAssets(), "christmaseve.ttf");
+        //Obtenemos la etiqueta de texto mediante el id, la guardamos en una variable del tipo TextView
+        // Lo buscamos por identificador mediante findViewById, este estara en la clase R donde estan todos los recursos
+        // Hacemos un casting para que devuelva un objeto TextView y no View
+        TextView tituloFeliz = findViewById(R.id.feliz);
+        TextView tituloNavidad = findViewById(R.id.navidad);
+        //Asociamos la etiqueta con la fuente mediante el metodo setTypeface
+        tituloFeliz.setTypeface(miFuente);
+        tituloNavidad.setTypeface(miFuente);
+
     }
 }
