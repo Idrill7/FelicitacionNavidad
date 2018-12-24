c95dd36f1cb9295e2d4feba916178e07d0ee6c0a
idril7 <alexuca@hotmail.es>
2018-12-24 18:17:03 +0100

    Añadido el Splash y el paso a la aplicacion

--- app/src/main/java/com/idrilplays/idril/a1_felicitacionnavidad/SplashActivity.java
+++ app/src/main/java/com/idrilplays/idril/a1_felicitacionnavidad/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.idrilplays.idril.a1_felicitacionnavidad;
 
+
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +13,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener{
     private TextView ho1;
     private TextView ho2;
     private TextView ho3;
@@ -45,6 +47,29 @@ public class SplashActivity extends AppCompatActivity {
         ho3.startAnimation(animacionHo);
         logo.startAnimation(animacionLogo);
 
+        //Ponemos la animacion a la escucha
+        // Escojo la ultima animacion, tendra como parametro el propio objeto, es una instancia de la clase que implementa la interfaz
+        animacionLogo.setAnimationListener(this);
+    }
+    //Metodos de la interfaz  implementada
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+    //Usaremos este metodo para que cuando acabe la animacion, se lance la activity main
+    @Override
+    public void onAnimationEnd(Animation animation) {
+    //Para cambiar de una activity a otra hay que crear in Intent
+        //Establecemos por parametro la "ruta" del origen y el destino
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        // Iniciamos el cambio de activity
+        startActivity(intent);
+        //Acabamos el Activity del splash ya que no vamos a retrodecer a ella
+        finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
 
     }
 }
